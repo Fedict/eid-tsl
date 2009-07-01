388218af6cbe09483605a45f16d2b6cdf7e5f435
frank.cornelis <frank.cornelis@be62d434-4484-11de-952f-710615d9d0d5>
2009-07-01 15:21:13 +0000

    fixed build for eID TSL Tool

--- eid-tsl-tool/src/main/java/be/fedict/eid/tsl/tool/SignSelectPkcs11FinishablePanel.java
+++ eid-tsl-tool/src/main/java/be/fedict/eid/tsl/tool/SignSelectPkcs11FinishablePanel.java
@@ -19,37 +19,53 @@
 package be.fedict.eid.tsl.tool;
 
 import java.awt.Component;
+import java.awt.FlowLayout;
+import java.io.File;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.event.ChangeListener;
-import javax.swing.text.html.StyleSheet.BoxPainter;
 
-import org.openide.WizardDescriptor.FinishablePanel;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openide.WizardValidationException;
+import org.openide.WizardDescriptor.ValidatingPanel;
 import org.openide.util.HelpCtx;
 
-public class SignSelectPkcs11FinishablePanel implements FinishablePanel<Object> {
+public class SignSelectPkcs11FinishablePanel implements ValidatingPanel<Object> {
 
-	@Override
-	public boolean isFinishPanel() {
-		return false;
-	}
+	private static final Log LOG = LogFactory
+			.getLog(SignSelectPkcs11FinishablePanel.class);
 
-	@Override
-	public void addChangeListener(ChangeListener listener) {
-	}
+	private JTextField pkcs11TextField;
+
+	private Component component;
 
 	@Override
 	public Component getComponent() {
-		JPanel panel = new JPanel();
-		BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.PAGE_AXIS);
-		panel.setLayout(boxLayout);
-		panel.add(new JLabel("Please select a PKCS#11 library."));
-		panel
-				.add(new JLabel(
-						"PKCS#11"));
-		return panel;
+		LOG.debug("get component");
+		if (null == this.component) {
+			/*
+			 * We need to return the same component each time, else the
+			 * validate() logic doesn't work as expected.
+			 */
+			JPanel panel = new JPanel();
+			BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.PAGE_AXIS);
+			panel.setLayout(boxLayout);
+			panel.add(new JLabel("Please select a PKCS#11 library."));
+
+			JPanel browsePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+			panel.add(browsePanel);
+			browsePanel.add(new JLabel("PKCS#11 library:"));
+			this.pkcs11TextField = new JTextField(30);
+			browsePanel.add(this.pkcs11TextField);
+			browsePanel.add(new JButton("Browse..."));
+			this.component = panel;
+		}
+		return this.component;
 	}
 
 	@Override
@@ -72,5 +88,24 @@ public class SignSelectPkcs11FinishablePanel implements FinishablePanel<Object> 
 
 	@Override
 	public void storeSettings(Object data) {
+	}
+
+	@Override
+	public void addChangeListener(ChangeListener listener) {
+	}
+
+	@Override
+	public void validate() throws WizardValidationException {
+		String pkcs11Library = this.pkcs11TextField.getText();
+		LOG.debug("PKCS#11 library: " + pkcs11Library);
+		File pkcs11File = new File(pkcs11Library);
+		if (false == pkcs11File.exists()) {
+			throw new WizardValidationException(null,
+					"PKCS#11 library not found", null);
+		}
+		if (true == pkcs11File.isDirectory()) {
+			throw new WizardValidationException(null,
+					"PKCS#11 library is a directory", null);
+		}
 	}
 }
